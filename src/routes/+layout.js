2b8028d8ae8e0523cf9e95046b71fb4f8fde1de9
Sandra Becker <sandra.becker1982@gmail.com>
2026-07-13 10:32:51 +0100

    delete speed-insights package from Vercel

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,5 @@
 import { dev } from '$app/environment';
 import { injectAnalytics } from '@vercel/analytics/sveltekit';
-import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 
 injectAnalytics({ mode: dev ? 'development' : 'production' });
-injectSpeedInsights();
+
